7d22a526d3174f3384e58b9cf7d7ec10628ace4f
amiof <arm131313@gmail.com>
2023-02-24 22:49:27 +0330

    fix logo in home background picture "

--- src/components/ChatBox/Chatbox.js
+++ src/components/ChatBox/Chatbox.js
@@ -3,7 +3,7 @@ import image from "../../pic/background-pic.jpg";
 function Chatbox() {
   return (
     <div
-      className="bg-cover w-full h-full bg-no-repeat bg-right text-white"
+      className="bg-cover w-full h-full bg-no-repeat bg-left text-white"
       style={{ backgroundImage: `URL(${image})` }}
     >
       Chatbox
